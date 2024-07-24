b7c9e71e8fe74673e8f57e0859b52774f04c59b7
arke0909 <arke0909@gmail.com>
2024-07-24 22:04:06 +0900

    Add_TextEffectSO&TextEffectList

---
+++ Assets/01.Scripts/TextEffects/TextEffectList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class TextEffectList : MonoBehaviour
+{
+    public List<TextEffectSO> _effects = new List<TextEffectSO>();
+}
---
+++ Assets/01.Scripts/TextViewer/TextViewer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextViewer : MonoBehaviour
+{
+    [SerializeField] private TextEffectList effectList;
+    
+    private void Next()
+    {
+        //TextEffectSO textEffect = 
+    }
+}
